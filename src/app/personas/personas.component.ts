097126e7a7759430135293d911519d0a1962bb5f
rjcortes <rjcortes@bancolombia.com.co>
2023-06-01 16:53:45 -0500

    Avance curso

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -10,9 +10,22 @@ export class PersonasComponent{
     deshabilitar = false;
     
     //Event Binding
-    mensaje: string = 'No se ha agregado ninguna persona';
+    mensaje = '';
+
+    //Event Binding (continuación)
+    titulo = 'Ingeniero';
+    
+    //*ngIf
+    mostrar = false;
 
     agregarPersona(){
         this.mensaje = 'Persona agregada';
+        this.mostrar = true;
     }
+
+    /*
+    modificarTitulo(event: Event){
+        this.titulo = (<HTMLInputElement> event.target).value;
+    }
+    */
 }
